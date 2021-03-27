a89f93a0047c58d67c43a29dd93c96f306abf189
marcosaureliodiasmoura <marcos_moura@live.com>
2021-03-27 14:29:54 -0300

    Hooks and Context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,16 +1,38 @@
-import React, {useContext} from 'react';
-
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container } from "./styles";
 
 export function Summary() {
-  const transactions = useContext(TransactionsContext);
+  const {transactions} = useTransactions();
 
-  console.log(transactions);
+  // const totalDeposits = transactions.reduce((acc, transaction) => {
+  //   if(transaction.type === 'deposit'){
+  //     return acc + transaction.amount;
+  //   }
+  //   return acc;
+  // },0)
+
+  const summary = transactions.reduce((acc, transaction) => { //ACC: Acumulator do reduce
+
+    console.log(transaction);
+
+    if(transaction.type === 'deposit'){
+      acc.deposits += transaction.amount; //+= soma para entradas
+      acc.total += transaction.amount; //resultado do total
+    }else{
+      acc.withdraws -= transaction.amount; //resultado de saídas
+      acc.total -= transaction.amount; //-+ subtrair
+    }
+    return acc; //o reduce precisa que toda alteração retorne com as mudanças o acc
+
+  },{ //reinicia como objetos
+    deposits: 0, 
+    withdraws: 0,
+    total: 0,
+  }) 
 
   return (
     <Container>
@@ -19,21 +41,37 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$ 1000,00</strong>
+          <strong>
+          {new Intl.NumberFormat('pt-BR',{
+                style: 'currency',
+                currency: 'BRL'
+              }).format(summary.deposits)}
+          </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$ 500,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR',{
+                style: 'currency',
+                currency: 'BRL'
+              }).format(summary.withdraws)}
+          </strong>
       </div>
       <div className="highlight">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$ 500,00</strong>
+        <strong>
+        {new Intl.NumberFormat('pt-BR',{
+                style: 'currency',
+                currency: 'BRL'
+              }).format(summary.total)}
+        </strong>
+       
       </div>
     </Container>
   )
